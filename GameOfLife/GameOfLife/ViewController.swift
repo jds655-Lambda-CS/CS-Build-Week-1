58e1523c36b3f82cb6ef4f4df614869500249523
Joshua Sharp <sharp.josh@gmail.com>
2020-08-19 23:25:15 -0400

    Base iOS project created

---
+++ GameOfLife/GameOfLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameOfLife
+//
+//  Created by Lambda_School_Loaner_214 on 8/19/20.
+//  Copyright © 2020 Empty Bliss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
